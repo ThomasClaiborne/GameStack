994ac19eb29980d87994977dccf38f759f79cd6a
Thomas Claiborne <thomas.claiborne123@gmail.com>
2026-05-07 15:05:49 -0400

    added result + resulttype

---
+++ app/Server/Domain/Result.cs
@@ -0,0 +1,16 @@
+namespace Server.Domain;
+
+public class Result<T>
+{
+    public List<string> Messages { get; set; } = new();
+    public ResultType Type { get; set; } = ResultType.Success;
+    public T? Payload { get; set; }
+
+    public bool IsSuccess => Type == ResultType.Success;
+
+    public void AddMessage(string message, ResultType type)
+    {
+        Messages.Add(message);
+        Type = type;
+    }
+}
---
+++ app/Server/Domain/ResultType.cs
@@ -0,0 +1,11 @@
+namespace Server.Domain;
+
+public enum ResultType
+{
+    Success,
+    NotFound,
+    BadRequest,
+    Unauthorized,
+    Forbidden,
+    Conflict
+}
